b322ef68761c95cec95d94bcf8b1ca6899d9886e
Tech-A33 <Tech-A33@tech-a33s-air.home>
2017-01-08 21:08:08 +0000

    functionally convert tictactoe from ES5 to ES6

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ class TicTacToe {
 
     //Player score and computer score which starts at zero
 
-     this.theButtons = $('#NandC');
+    this.theButtons = $('#NandC');
     this.player1ScoreCount = 0;
     this.player2ScoreCount = 0;
     this.player2 = false;
@@ -33,99 +33,103 @@ class TicTacToe {
   }
 
   playerMove(target) {
- 
-  if (this.player1) {
-    this.player2 = true;
-    this.player1 = false;
-    //this.playerOption = target.target
-    $(target.target).text("X").css('font-size', '90px', 'text-align', 'center');
-  } else {
-    this.player1 = true;
-    this.player2 = false;
-    $(target.target).text("O").css('font-size', '90px', 'text-align', 'center');
+
+    if (this.player1) {
+      this.player2 = true;
+      this.player1 = false;
+      //this.playerOption = target.target
+     
+      console.log(target.target);
+      $(target.target).text("X").css('font-size', '90px', 'text-align', 'center');
+    } else {
+      this.player1 = true;
+      this.player2 = false;
+      $(target.target).text("O").css('font-size', '90px', 'text-align', 'center');
+    }
+    this.getMoveSaved();
   }
-  this.getMoveSaved();
-}
 
- getMoveSaved() {
- this.tablerow = $("tbody tr", $("#NandC")).map(function () {
-   return [$("td", this).map(function () {
-     return this.innerHTML;
-   }).get()];
- }).get();
-  this.checkDiagonal();
-}
-
-checkDiagonal() {
-   this.tabledia1 = [this.tablerow[0][0], this.tablerow[1][1], this.tablerow[2][2]];
-   this.tabledia2 = [this.tablerow[0][2], this.tablerow[1][1], this.tablerow[2][0]];
-   //console.log(this.tabledia1);
+  getMoveSaved() {
+    this.tablerow = $("tbody tr", $("#NandC")).map(function () {
+      return [$("td", this).map(function () {
+        return this.innerHTML;
+      }).get()];
+    }).get();
+    this.checkDiagonal();
+  }
+  arrayColumn(arr, n) {
+    return arr.map(x => x[n]);
+  }
+  checkDiagonal() {
+    this.tabledia1 = [this.tablerow[0][0], this.tablerow[1][1], this.tablerow[2][2]];
+    this.tabledia2 = [this.tablerow[0][2], this.tablerow[1][1], this.tablerow[2][0]];
+    //console.log(this.tabledia1);
     //console.log(this.NoughtsWinningCombo);
-   this.tabledia1.toString() === this.CrossesWinningCombo.toString() ? this.Player1Output() :
-   this.tabledia2.toString() === this.CrossesWinningCombo.toString() ? this.Player1Output() :
-   this.tabledia1.toString() === this.NoughtsWinningCombo.toString() ? this.Player2Output() :
-   this.tabledia2.toString() === this.NoughtsWinningCombo.toString() ? this.Player2Output() : this.checkColumns();
-
-
- }
-
- checkColumns() {
-
-  for (let column in this.tablerow) {
-    if (arrayColumn(this.tablerow, column).toString() === CrossesWinningCombo.toString()) {
-      this.Player1Output();
-    } else if (arrayColumn(this.tablerow, column).toString() === NoughtsWinningCombo.toString()) {
-      this.Player2Output();
-    } else {
-      this.checkRows();
-    }
-  }
-}
-
- checkRows() {
-  for (i = 0; i <= this.tablerow.length; i++) {
-    var rowvalue = this.tablerow[i];
-    console.log(rowvalue);
-    if (rowvalue.toString() === this.CrossesWinningCombo.toString()) {
-      this.Player1Output();
-    } else if (rowvalue.toString() === this.NoughtsWinningCombo.toString()) {
-      this.Player2Output();
-    } else {
-      null;
-    }
+    this.tabledia1.toString() === this.CrossesWinningCombo.toString() ? this.Player1Output() :
+      this.tabledia2.toString() === this.CrossesWinningCombo.toString() ? this.Player1Output() :
+        this.tabledia1.toString() === this.NoughtsWinningCombo.toString() ? this.Player2Output() :
+          this.tabledia2.toString() === this.NoughtsWinningCombo.toString() ? this.Player2Output() : this.checkColumns();
 
 
   }
-}
 
-//A function that perform actions if the Player1 wins
- Player1Output() {
-  $("#resultmessage").html("Player1 wins");
-  this.player1ScoreCount++;
-  let theScore = document.getElementById("Player1count").innerHTML = this.player1ScoreCount;
-  console.log(this.player1ScoreCount);
-  this.resetTheBoard();
-}
-//A function that perform actions if the player2 wins
- Player2Output() {
-  $("#resultmessage").html("Player2 wins");
-  this.player2ScoreCount++;
-  let theScore = document.getElementById("Player2count").innerHTML = this.player1ScoreCount;
-  console.log(this.player1ScoreCount);
-  this.resetTheBoard();
-}
-//function if the match is a draw
- matchIsADraw() {
-  $("#resultmessage").html("its a draw");
-  $("#NandC tbody tr td").html("");
-  this.resetTheBoard();
-}
-//function to reset the board
- resetTheBoard() {
-  $("#NandC tbody tr td").html("");
-  counter = 0;
-  console.log(counter);
-}
+  checkColumns() {
+
+    for (let column in this.tablerow) {
+      if (this.arrayColumn(this.tablerow, column).toString() === this.CrossesWinningCombo.toString()) {
+        this.Player1Output();
+      } else if (this.arrayColumn(this.tablerow, column).toString() === this.NoughtsWinningCombo.toString()) {
+        this.Player2Output();
+      } else {
+        this.checkRows();
+      }
+    }
+  }
+
+  checkRows() {
+    for (let i = 0; i <= this.tablerow.length; i++) {
+      var rowvalue = this.tablerow[i];
+      console.log(rowvalue);
+      if (rowvalue.toString() === this.CrossesWinningCombo.toString()) {
+        this.Player1Output();
+      } else if (rowvalue.toString() === this.NoughtsWinningCombo.toString()) {
+        this.Player2Output();
+      } else {
+        null;
+      }
+
+
+    }
+  }
+
+  //A function that perform actions if the Player1 wins
+  Player1Output() {
+    $("#resultmessage").html("Player1 wins");
+    this.player1ScoreCount++;
+    let theScore = document.getElementById("Player1count").innerHTML = this.player1ScoreCount;
+    console.log(this.player1ScoreCount);
+    this.resetTheBoard();
+  }
+  //A function that perform actions if the player2 wins
+  Player2Output() {
+    $("#resultmessage").html("Player2 wins");
+    this.player2ScoreCount++;
+    let theScore = document.getElementById("Player2count").innerHTML = this.player1ScoreCount;
+    console.log(this.player1ScoreCount);
+    this.resetTheBoard();
+  }
+  //function if the match is a draw
+  matchIsADraw() {
+    $("#resultmessage").html("its a draw");
+    $("#NandC tbody tr td").html("");
+    this.resetTheBoard();
+  }
+  //function to reset the board
+  resetTheBoard() {
+    $("#NandC tbody tr td").html("");
+    this.counter = 0;
+    console.log(this.counter);
+  }
 
 
 }
