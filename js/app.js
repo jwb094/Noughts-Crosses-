add71226cb4058e834d614cb061d4e83af70329e
Tech-A33 <Tech-A33@tech-a33s-air.home>
2017-01-08 20:33:09 +0000

    convert code to ES6: now debug to fix errors;

--- js/app.js
+++ js/app.js
@@ -31,20 +31,6 @@ class TicTacToe {
     });
   }
 
-//    ButtonEventListener() {
-//     $(this.theButtons).click((this.play.bind(this.e)));
-//       this.counter++;
-//       if (this.counter >= 8) {
-//         matchIsADraw();
-//       }
-//     }
-  
-
-// play(){
-//   this.playerMove();
-//   this.getMoveSaved();
-// }
-
   playerMove(target) {
  
   if (this.player1) {
@@ -56,81 +42,49 @@ class TicTacToe {
     this.player2 = false;
     $(target.target).text("O").css('font-size', '90px', 'text-align', 'center');
   }
-  getMoveSaved();
+  this.getMoveSaved();
 }
 
-//function getMoveSaved() {
- // tablerow = $("tbody tr", $("#NandC")).map(function () {
- //   return [$("td", this).map(function () {
-   //   return this.innerHTML;
-   // }).get()];
- // }).get();
-  //checkDiagonal();
-//}
-
-
-
-
+ getMoveSaved() {
+ this.tablerow = $("tbody tr", $("#NandC")).map(function () {
+   return [$("td", this).map(function () {
+     return this.innerHTML;
+   }).get()];
+ }).get();
+  this.checkDiagonal();
 }
 
+checkDiagonal() {
+   const tabledia1 = [this.tablerow[0][0], this.tablerow[1][1], this.tablerow[2][2]];
+   const tabledia2 = [this.tablerow[0][2], this.tablerow[1][1], this.tablerow[2][0]];
+   tabledia1.toString() === this.CrossesWinningCombo.toString() ? this.Player1Output() :
+   tabledia2.toString() === this.CrossesWinningCombo.toString() ? this.Player1Output() :
+   tabledia1.toString() === this.NoughtsWinningCombo.toString() ? this.Player2Output() :
+   tabledia2.toString() === this.NoughtsWinningCombo.toString() ? this.Player2Output() : this.checkColumns();
 
+ }
 
+//  checkColumns() {
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//function get column of array
-//para1 array - Object
-//para2 number - int
-//returns array column
-//function arrayColumn(arr, n) {
-//  return arr.map(x => x[n]);
-//}
-
-// function turns table in from end into an array
-//function getMoveSaved() {
- // tablerow = $("tbody tr", $("#NandC")).map(function () {
- //   return [$("td", this).map(function () {
-   //   return this.innerHTML;
-   // }).get()];
- // }).get();
-  //checkDiagonal();
-//}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-// function checkDiagonal() {
-//   let tabledia1 = [tablerow[0][0], tablerow[1][1], tablerow[2][2]];
-//   let tabledia2 = [tablerow[0][2], tablerow[1][1], tablerow[2][0]];
-//   tabledia1.toString() === CrossesWinningCombo.toString() ? Player1Output() :
-//     tabledia2.toString() === CrossesWinningCombo.toString() ? Player1Output() :
-//       tabledia1.toString() === NoughtsWinningCombo.toString() ? Player2Output() :
-//         tabledia2.toString() === NoughtsWinningCombo.toString() ? Player2Output() : checkColumns();
-
-// }
-
-// function checkColumns() {
-
-//   for (let column in tablerow) {
+//   for (let column in this.tablerow) {
 //     if (arrayColumn(tablerow, column).toString() === CrossesWinningCombo.toString()) {
-//       Player1Output();
+//       this.Player1Output();
 //     } else if (arrayColumn(tablerow, column).toString() === NoughtsWinningCombo.toString()) {
-//       Player2Output();
+//       this.Player2Output();
 //     } else {
-//       checkRows();
+//       this.checkRows();
 //     }
 //   }
 // }
 
-// function checkRows() {
-//   for (i = 0; i <= tablerow.length; i++) {
-//     var rowvalue = tablerow[i];
+//  checkRows() {
+//   for (i = 0; i <= this.tablerow.length; i++) {
+//     var rowvalue = this.tablerow[i];
 //     //console.log(rowvalue);
 //     if (rowvalue.toString() === CrossesWinningCombo.toString()) {
-//       Player1Output();
+//       this.Player1Output();
 //     } else if (rowvalue.toString() === NoughtsWinningCombo.toString()) {
-//       Player2Output();
+//       this.Player2Output();
 //     } else {
 //       null;
 //     }
@@ -139,34 +93,34 @@ class TicTacToe {
 //   }
 // }
 
-
-// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// //A function that perform actions if the Player1 wins
-// function Player1Output() {
-//   $("#resultmessage").html("Player1 wins");
-//   player1ScoreCount++;
-//   let theScore = document.getElementById("Player1count").innerHTML = player1ScoreCount;
-//   console.log(player1ScoreCount);
-//   resetTheBoard();
-// }
-// //A function that perform actions if the player2 wins
-// function Player2Output() {
-//   $("#resultmessage").html("Player2 wins");
-//   player2ScoreCount++;
-//   let theScore = document.getElementById("Player2count").innerHTML = player2ScoreCount;
-//   console.log(player2ScoreCount);
-//   resetTheBoard();
-// }
-// //function if the match is a draw
-// function matchIsADraw() {
-//   $("#resultmessage").html("its a draw");
-//   $("#NandC tbody tr td").html("");
-//   resetTheBoard();
-// }
+//A function that perform actions if the Player1 wins
+ Player1Output() {
+  $("#resultmessage").html("Player1 wins");
+  this.player1ScoreCount++;
+  let theScore = document.getElementById("Player1count").innerHTML = player1ScoreCount;
+  console.log(player1ScoreCount);
+  this.resetTheBoard();
+}
+//A function that perform actions if the player2 wins
+ Player2Output() {
+  $("#resultmessage").html("Player2 wins");
+  this.player2ScoreCount++;
+  let theScore = document.getElementById("Player2count").innerHTML = player2ScoreCount;
+  console.log(player2ScoreCount);
+  this.resetTheBoard();
+}
+//function if the match is a draw
+ matchIsADraw() {
+  $("#resultmessage").html("its a draw");
+  $("#NandC tbody tr td").html("");
+  this.resetTheBoard();
+}
 // //function to reset the board
-// function resetTheBoard() {
+//  resetTheBoard() {
 //   $("#NandC tbody tr td").html("");
 //   counter = 0;
 //   console.log(counter);
 // }
-// });*/
+
+
+}
